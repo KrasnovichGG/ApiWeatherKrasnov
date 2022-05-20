b161b443117f47ccb21251185c7fbf41c25e8185
Alexsunder <Sasha-kr90@bk.ru>
2022-05-20 09:16:57 +0300

    I made a Pin and a Location to Kazan combined with the weather and made the cancellation of actions after 15 seconds(Сделал Pin и Локацию к Казани объединил с погодой и сделал отмену действий после 15 секунд)

--- ApiWeatherKrasnov/ApiWeatherKrasnov/Views/TodoListPage.xaml.cs
+++ ApiWeatherKrasnov/ApiWeatherKrasnov/Views/TodoListPage.xaml.cs
@@ -3,8 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace ApiWeatherKrasnov.Views
@@ -12,11 +15,38 @@ namespace ApiWeatherKrasnov.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TodoListPage : ContentPage
     {
+        CancellationTokenSource stc;
+        Pin krasnovichPin = new Pin();
         public TodoListPage()
         {
             InitializeComponent();
+            var res = GetLocation();
+            krasnovichPin.Label = "Техникум Связи";
+            krasnovichPin.Address = "KTITS";
+            krasnovichPin.Position = new Position(55.8016314,49.175043);
+            MyMap.Pins.Add(krasnovichPin);
         }
-   
+        async Task<Location> GetLocation()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(15));
+            stc = new CancellationTokenSource();
+            var location = await Geolocation.GetLocationAsync(request,stc.Token);
+            if (location != null)
+            {
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), new Distance(100d)));
+            }
+            return location;
+
+        }
+        protected override void OnAppearing()
+        {
+            if(stc != null && !stc.IsCancellationRequested)
+            {
+                stc.Cancel();
+            }
+            base.OnAppearing();
+        }
+
         private async void SearchBar_SearchButtonPressed(object sender, EventArgs e)
         {
             try
@@ -25,9 +55,9 @@ namespace ApiWeatherKrasnov.Views
                 BindingContext = a.main;
                 Temp.Text += "°C - градусов цельсия";
                 Fells.Text += "°C - ощущение";
-                Wind.Text += "м.с ветер!";
-                Hum.Text += "г/м³ - Влажность";
-                Pressure.Text += "атм Давление";
+                Wind.Text += $"{a.wind.speed} - м.с ветер!";
+                Hum.Text += " - г/м³Влажность";
+                Pressure.Text += " - атм Давление";
 
             }
             catch (Exception ex)
